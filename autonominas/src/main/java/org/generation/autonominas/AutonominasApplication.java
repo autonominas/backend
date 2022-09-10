dde7f96c6f15dfe57fb1ba8e02fb7c92e932b6c0
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-09 23:04:35 -0300

    Cria o projeto Spring

---
+++ autonominas/src/main/java/org/generation/autonominas/AutonominasApplication.java
@@ -0,0 +1,13 @@
+package org.generation.autonominas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonominasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonominasApplication.class, args);
+	}
+
+}
---
+++ autonominas/src/test/java/org/generation/autonominas/AutonominasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.autonominas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonominasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

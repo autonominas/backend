4a33a6444db96db5a38cef99d0fd4e4aafaf045c
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-11 20:05:09 -0300

    Cria as classes Tema e Postagem na Model

---
+++ autonominas/src/main/java/org/generation/autonominas/model/Postagem.java
@@ -0,0 +1,65 @@
+package org.generation.autonominas.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagem")
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O título é obrigatório!")
+	@Size(min = 5, max = 100, message = "O título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotNull(message = "O conteúdo é obrigatório!")
+	private String conteudo;
+	
+	@UpdateTimestamp
+	private LocalDateTime data_post;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public LocalDateTime getData_post() {
+		return data_post;
+	}
+
+	public void setData_post(LocalDateTime data_post) {
+		this.data_post = data_post;
+	}
+	
+}
---
+++ autonominas/src/main/java/org/generation/autonominas/model/Tema.java
@@ -0,0 +1,37 @@
+package org.generation.autonominas.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="tb_tema")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull 
+	private String tipo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}

3443c94852f6c467de56dd10eb291311c06861ed
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-11 21:20:07 -0300

    Cria Relacionamento one to many

--- autonominas/src/main/java/org/generation/autonominas/model/Tema.java
+++ autonominas/src/main/java/org/generation/autonominas/model/Tema.java
@@ -1,11 +1,17 @@
 package org.generation.autonominas.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_tema")
@@ -17,6 +23,18 @@ public class Tema {
 	
 	@NotNull 
 	private String tipo;
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+	
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 
 	public Long getId() {
 		return id;

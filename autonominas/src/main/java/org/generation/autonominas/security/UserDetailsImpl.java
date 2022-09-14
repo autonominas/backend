fb7b48cf31ff303ea53d2be145e2a459c9bb527e
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-14 13:27:54 -0300

    Cria a camada de segurança da aplicação

---
+++ autonominas/src/main/java/org/generation/autonominas/security/UserDetailsImpl.java
@@ -0,0 +1,70 @@
+package org.generation.autonominas.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.generation.autonominas.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+	
+	private static final long serialVersionUID = 1L;
+	
+		private String userName;
+		private String password;
+		
+		private List<GrantedAuthority> authorities;
+		
+		public UserDetailsImpl (Usuario user) {
+			this.userName = user.getUsuario();
+			this.password = user.getSenha();
+		}
+		
+		public UserDetailsImpl () {}
+
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			
+			return null;
+		}
+
+		@Override
+		public String getPassword() {
+			
+			return null;
+		}
+
+		@Override
+		public String getUsername() {
+		
+			return null;
+		}
+
+		@Override
+		public boolean isAccountNonExpired() {
+			
+			return false;
+		}
+
+		@Override
+		public boolean isAccountNonLocked() {
+			
+			return false;
+		}
+
+		@Override
+		public boolean isCredentialsNonExpired() {
+			
+			return false;
+		}
+
+		@Override
+		public boolean isEnabled() {
+			
+			return false;
+		}
+		
+		
+
+}

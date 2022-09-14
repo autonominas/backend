890e40e459c739fa062929eeac43272993db0b6e
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-14 12:09:08 -0300

    Adiciona relacionamento one to many com a classe Postagem

--- autonominas/src/main/java/org/generation/autonominas/model/Usuario.java
+++ autonominas/src/main/java/org/generation/autonominas/model/Usuario.java
@@ -1,13 +1,19 @@
 package org.generation.autonominas.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -29,6 +35,10 @@ public class Usuario {
 	
 	@Size(max = 5000, message = "O link da foto não pode sser maior do que 5000 caracteres")
 	private String foto;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 
 	public Long getId() {
 		return id;
